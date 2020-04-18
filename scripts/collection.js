1056ce496e3790d3bc43720056125be193504052
Jurena <jju216@nyu.edu>
2020-04-18 10:52:45 -0400

    added archive images and clothing onto grids

--- scripts/collection.js
+++ scripts/collection.js
@@ -10,7 +10,9 @@ var docFrag = document.createDocumentFragment();
 //put placeholder image in image var for now, append to imgcol
 var imgCol; 
 var img;
-var emptyCol; 
+//curently holds all apparel, must be curated
+var appImgs = ["1976-1989 Black Engineers.png","1976-1989 Text Styalized.png","1976-1989.png","1977 Student.png","1977 Stylized.png","1982-89 Stylized.png","76-89 Text 1.png","76-89 Text 2.png","76-89 Text 3.png","76-89 Text 4.png","Alphas Cropped.png","Alphas.png","ChemE 2.png","ChemE.png","Counterweight.png","German - PW.png","German - Sitins.png","NSBE 1982-89.png","Poets.png","T-Shirt WINO.png","WINO Expansion.png"]; 
+appImgs.sort(); 
 
 //console.log(img); 
 
@@ -19,17 +21,17 @@ var emptyCol;
 console.log(docFrag); 
 function clothes_grid(){
 	"use strict"; 
-	for (var i=0; i<12; i++){
+	for (var i=0; i<4; i++){
 		newRow = document.createElement("div"); 
-		newRow.classList.add("columns", "is-multiline", "is-mobile", "is-centered", "is-vcentered"); 
-		for (var ind=0; ind<4; ind++){
+		newRow.classList.add("columns", "is-multiline", "is-mobile", "is-centered", "has-margin-right-75", "has-margin-left-75"); 
+		for (var ind=0; ind<3; ind++){
 //			emptyCol = document.createElement("div");
 //			emptyCol.classList.add("column", "is-narrow");
 			imgCol = document.createElement("div"); 
-			imgCol.classList.add("column", "is-one-third", "is-one-third-mobile", "is-half");
+			imgCol.classList.add("column", "image");
 			img = document.createElement("img"); 
-			img.src = "../Website Files/img/plchldr.png"; 
-//			imgCol.appendChild(img); 
+			img.src = "../Tandon-Poly-Archives-Project/img/Clothing Images/" + appImgs.shift();  
+			imgCol.appendChild(img); 
 //			newRow.appendChild(emptyCol); 
 			newRow.appendChild(imgCol); 
 		}
